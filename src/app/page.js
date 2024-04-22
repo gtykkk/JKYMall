59c5382478fee26bf66bd6b16e04e900e26c9d2d
cbykkk <cbdragoncb@gmail.com>
2024-04-22 21:28:08 +0900

    cby's first commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      작업 시작할 거임
-    </main>
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        작업 시작할 거임
+        <div className="bg-white text-black">
+          <p>My first commit</p>  
+        </div>
+      </main>
+      
+    </>
   );
 }
